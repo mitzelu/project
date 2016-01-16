187fa32767b26f8d03144a3026f3b76fdb1398f4
Luminita <luminita.lh@gmail.com>
2016-01-16 18:05:54 +0100

    Update urls.py

--- myDoc/urls.py
+++ myDoc/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^http://bookadoctor.cloudapp.net/admin/', include(admin.site.urls)),
-    url(r'^http://bookadoctor.cloudapp.net/doctors/', include('doctors.urls', namespace='doctors')),
-    url(r'^http://bookadoctor.cloudapp.net/search/$', 'doctors.views.search'),
-    url(r'^http://bookadoctor.cloudapp.net/booking/(?P<doctor_id>\d+)/$', 'doctors.views.booking_form'),
-    url(r'^http://bookadoctor.cloudapp.net/bookingsucced/(?P<doctor_id>\d+)/$', 'doctors.views.booking'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^doctors/', include('doctors.urls', namespace='doctors')),
+    url(r'^search/$', 'doctors.views.search'),
+    url(r'^(?P<doctor_id>\d+)/$', 'doctors.views.booking_form'),
+    url(r'^bookingsucced/(?P<doctor_id>\d+)/$', 'doctors.views.booking'),
 ]
